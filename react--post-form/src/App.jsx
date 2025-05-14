1378443fe85bc4f1af55c86be8c48dc2210a0dbd
ErikaArci99 <erikaarci1999@outlook.it>
2025-05-14 15:42:09 +0200

    [UPD] aggiunte funzionalità in app.jsx

--- react--post-form/src/App.jsx
+++ react--post-form/src/App.jsx
@@ -1,18 +1,20 @@
-import { useState } from 'react'
-import Header from './components/Header'
-import PostForm from './components/PostForm'
-import PostCards from './components/PostCards'
-import './App.css'
+import { useState } from 'react';
+import Header from './components/Header';
+import PostForm from './components/PostForm';
+import PostCards from './components/PostCards';
+import './App.css';
 
 function App() {
+  const [postData, setPostData] = useState(null);
+
   return (
     <>
       <Header />
-      <div className="container d-flex justify-content-center mt-5">
-        <PostForm />
+      <div className="container d-flex justify-content-center">
+        <PostForm setPostData={setPostData} />
       </div>
-      <div className="container d-flex justify-content-center mt-5">
-        <PostCards />
+      <div className="container d-flex justify-content-center">
+        <PostCards postData={postData} />
       </div>
     </>
   );
--- react--post-form/src/components/PostCards.jsx
+++ react--post-form/src/components/PostCards.jsx
@@ -5,8 +5,8 @@ const PostCards = () => {
         <>
             <div className="container">
                 <div className="row">
-                    <div className="col-12 col-lg-4 mt-4">
-                        <div className="card">
+                    <div className="col-12 col-lg-4">
+                        <div className="card m-3">
                             <div className="card-header">Titolo del post</div>
                             <div className="card-body">
                                 <div className="mb-2">
