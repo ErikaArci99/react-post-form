9e4faa95d77faccf7b2ec923523965878b1c9683
ErikaArci99 <erikaarci1999@outlook.it>
2025-05-14 14:48:14 +0200

    [UPD] create componenti e importate in app.jsx

--- react--post-form/src/App.jsx
+++ react--post-form/src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
+import PostForm from './components/PostForm'
+import PostCards from './components/PostCards'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-
-    </>
-  )
+    <div className="container-fluid mt-5">
+      <div className="row">
+        <div className="col-12 col-lg-8">
+          <form>
+            < PostForm />
+          </form>
+        </div>
+        <PostCards />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ react--post-form/src/components/PostCards.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PostCards = () => {
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default PostCards
