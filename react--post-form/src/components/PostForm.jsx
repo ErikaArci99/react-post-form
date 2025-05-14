660116ef5a3ead6b0850c811a754d644363f51ca
ErikaArci99 <erikaarci1999@outlook.it>
2025-05-14 14:48:48 +0200

    [UPD] creata struttura statica form

--- react--post-form/src/components/PostForm.jsx
+++ react--post-form/src/components/PostForm.jsx
@@ -2,9 +2,42 @@ import React from 'react'
 
 const PostForm = () => {
     return (
-        <>
+        <div className="row g-3">
+            <div className="col-12 col-md-6">
+                <input
+                    type="text"
+                    placeholder="Autore"
+                    className="form-control"
+                />
+            </div>
+            <div className="col-12 col-md-6">
+                <input
+                    type="text"
+                    placeholder="Titolo"
+                    className="form-control"
+                />
+            </div>
+            <div className="col-12">
+                <textarea
+                    placeholder="Testo del post"
+                    className="form-control"
+                    rows="5"
+                ></textarea>
+            </div>
+            <div className="col-12">
+                <div className="form-check">
+                    <input
+                        className="form-check-input"
+                        type="checkbox"
+                        id="publicPost"
+                    />
+                    <label className="form-check-label" htmlFor="publicPost">
+                        Rendi pubblico
+                    </label>
+                </div>
+            </div>
+        </div>
 
-        </>
     )
 }
 
